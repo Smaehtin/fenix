84e5ee86c01205c231025504c74f2247e6119f24
Smaehtin <26857259+Smaehtin@users.noreply.github.com>
2022-04-01 22:40:38 +0200

    Use fixed APK path for beta installer to overwrite same file on update

--- automation/termux/install-latest-beta.py
+++ automation/termux/install-latest-beta.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python
- 
-import re
+
 import os
 import requests
 import sys
- 
-def get_file_name(response):
-    return re.findall(
-        'filename=(.+)',
-        response.headers['content-disposition']
-    )[0]
- 
+
 def main():
     home = os.getenv('HOME')
- 
+
     response = requests.get(
         'https://api.github.com/repos/Smaehtin/fenix/releases',
         headers={
@@ -24,7 +17,7 @@ def main():
 
     assets = data[0]['assets']
     download_url = assets[0]['browser_download_url']
- 
+
     file_response = requests.get(download_url)
     if not file_response.ok:
         print(
@@ -32,12 +25,15 @@ def main():
             file=sys.stderr
         )
         return
- 
-    file_name = get_file_name(file_response)
-    destination = f'{home}/storage/downloads/{file_name}'
- 
+
+    destination = f'{home}/storage/downloads/fenix/beta.apk'
+
+    os.makedirs(
+        os.path.dirname(destination),
+        exist_ok=True
+    )
     open(destination, 'wb').write(file_response.content)
- 
+
     os.system(f'termux-share {destination}')
- 
+
 main()
